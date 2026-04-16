36d3306e2f7528be56c592fba264dc9a7801e90c
SevenLawless <7lawless25@gmail.com>
2026-04-16 20:25:19 +0100

    all

--- week_5/day_3/daily_challenge/challenges.js
+++ week_5/day_3/daily_challenge/challenges.js
@@ -1,11 +1,17 @@
-//------------ex1-----------------
+productSchema.index({ category: 1, price: 1 })
 
-//------------ex2-----------------
+const getProducts = async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = 24
+  
+    const products = await Product.find()
+      .select("title price image")
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean()
+  
+    res.json(products)
+  }
 
-//------------ex3-----------------
 
-//------------ex4-----------------
-
-//------------ex5-----------------
-
-//------------ex6-----------------
+  
